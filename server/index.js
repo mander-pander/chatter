ecb723431e057643be08d4602c6d7ede408726e5
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-01-24 19:15:56 -0800

    import the http and cors library, add socket.io, add script for nodemon

--- server/index.js
+++ server/index.js
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
+});
+
 app.get('/api', (req, res) => {
   res.json({
     message: 'Hello world',
